d4311b26bb6dd499d7c0d2e8c34bc573d6216b8b
Daniel Holland <danhol1989@gmail.com>
2022-03-21 19:04:04 -0700

    Added userData seed. Updated other seeds.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,21 @@
 const sequelize = require('../config/connection');
-const { Blog } = require('../models');
+const { Blog, User } = require('../models');
 
 const blogData = require('./blogData.json');
+const userData = require('./userData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
+  const users = await User.create(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
   for (const blog of blogData) {
     await Blog.create({
       ...blog,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+      user_id: users[0].id,
     });
   }
 
