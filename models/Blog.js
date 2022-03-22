9428a0715d3e7854f4f6410cc3221c0eb27ed408
Daniel Holland <danhol1989@gmail.com>
2022-03-21 18:50:42 -0700

    Updated models

--- models/Blog.js
+++ models/Blog.js
@@ -15,9 +15,6 @@ Blog.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
-      type: DataTypes.STRING,
-    },
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
